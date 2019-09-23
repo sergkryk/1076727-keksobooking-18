fab7c91aabe6c9f073820483778f355806855dec
sergkryk <41703448+sergkryk@users.noreply.github.com>
2019-09-23 15:28:39 +0300

    переделал по замечаниям реквеста

--- js/main.js
+++ js/main.js
@@ -18,12 +18,21 @@ var MOCK = {
   offer: {
     title: ['Доступное жильё', 'Шикарные аппартаменты', 'Семейное гнёздышко', 'Хостел', 'Роскошная студия с видом на море'],
     address: {
-      // min: randomNumber(300, 650),
-      // max: randomNumber(300, 650)
+      min: 100,
+      max: 700
     },
-    // price: randomNumber(10, 5000),
-    // rooms: randomNumber(1, 10),
-    // guests: randomNumber(1, 20),
+    price: {
+      min: 1000,
+      max: 20000
+    },
+    rooms: {
+      min: 1,
+      max: 10
+    },
+    guests: {
+      min: 1,
+      max: 25
+    },
     type: ['palace', 'flat', 'house', 'bungalo'],
     checkin: ['12:00', '13:00', '14:00'],
     checkout: ['12:00', '13:00', '14:00'],
@@ -32,8 +41,14 @@ var MOCK = {
     photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
   },
   location: {
-    // x: randomNumber(60, 1030),
-    // y: randomNumber(130, 630)
+    x: {
+      min: 60,
+      max: 1030
+    },
+    y: {
+      min: 130,
+      max: 630
+    }
   }
 };
 
@@ -46,14 +61,11 @@ var generateContent = function (array) {
       },
       offer: {
         title: array.offer.title[randomNumber(0, array.offer.title.length - 1)],
-        address: {
-          min: randomNumber(300, 650),
-          max: randomNumber(300, 650),
-        },
-        price: randomNumber(10, 5000),
+        address: randomNumber(array.offer.address.min, array.offer.address.max) + ', ' + randomNumber(array.offer.address.min, array.offer.address.max),
+        price: randomNumber(array.offer.price.min, array.offer.price.max),
         type: array.offer.type[randomNumber(0, array.offer.type.length - 1)],
-        rooms: randomNumber(1, 10),
-        guests: randomNumber(1, 20),
+        rooms: randomNumber(array.offer.rooms.min, array.offer.rooms.max),
+        guests: randomNumber(array.offer.guests.min, array.offer.guests.max),
         checkin: array.offer.checkin[randomNumber(0, array.offer.checkin.length - 1)],
         checkout: array.offer.checkout[randomNumber(0, array.offer.checkout.length - 1)],
         features: array.offer.features[randomNumber(0, array.offer.features.length - 1)],
@@ -61,8 +73,8 @@ var generateContent = function (array) {
         photos: array.offer.photos[randomNumber(0, array.offer.photos.length - 1)]
       },
       location: {
-        x: randomNumber(60, 1030),
-        y: randomNumber(130, 630)
+        x: randomNumber(array.location.x.min, array.location.x.max),
+        y: randomNumber(array.location.y.min, array.location.y.max)
       }
     };
   }
@@ -71,17 +83,17 @@ var generateContent = function (array) {
 
 var generatePins = function (array) {
   var element = mapPin.cloneNode(true);
-  element.style.left = (array[i].location.x - 25) + 'px';
-  element.style.top = (array[i].location.y - 70) + 'px';
-  element.querySelector('img').src = array[i].author.avatar;
-  element.querySelector('img').alt = array[i].offer.title;
+  element.style.left = (array.location.x - 25) + 'px';
+  element.style.top = (array.location.y - 70) + 'px';
+  element.querySelector('img').src = array.author.avatar;
+  element.querySelector('img').alt = array.offer.title;
   fragment.appendChild(element);
 };
 
 var mockArray = generateContent(MOCK);
 
-for (var i = 0; i < advertsCount; i++) {
-  generatePins(mockArray);
+for (var i = 0; i < mockArray.length; i++) {
+  generatePins(mockArray[i]);
 }
 
 map.classList.remove('map--faded');
