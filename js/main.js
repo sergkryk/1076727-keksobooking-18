57fb9d2114346f59e974b699da8ffa7733a3d7c3
sergkryk <41703448+sergkryk@users.noreply.github.com>
2019-10-06 22:02:56 +0300

    закончил задание

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
 var PIN_RADIUS = 32.5;
 var PIN_HEIGTH = 87;
 var advertsCount = 8;
@@ -8,24 +9,31 @@ var mapPin = document.querySelector('#pin').content.querySelector('.map__pin');
 var mapPinMain = document.querySelector('.map__pin--main');
 var pinList = map.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
-// var pinCard = document.querySelector('#card').content.querySelector('.map__card');
-// var filtersContainer = map.querySelector('.map__filters-container');
-// var cardPhoto = document.querySelector('#card').content.querySelector('.popup__photo');
+var pinCard = document.querySelector('#card').content.querySelector('.map__card');
+var cardPhoto = document.querySelector('#card').content.querySelector('.popup__photo');
 var yourAdForm = document.querySelector('.ad-form');
 var yourAdFormFields = yourAdForm.querySelectorAll('fieldset');
 var addressInput = document.querySelector('#address');
+// для подсчёта количества гостей
 var roomCapacity = document.querySelector('#capacity');
 var roomNumber = document.querySelector('#room_number');
-// var roomList = roomNumber.querySelectorAll('option');
 var capacityList = roomCapacity.querySelectorAll('option');
-// var adFormSubmitButton = document.querySelector('.ad-form__submit');
+var roomTypeList = document.querySelector('#type').querySelectorAll('option');
+// для установления минимальной цены в зависимости от типа аппартаментов
+var roomType = document.querySelector('#type');
+var roomPrice = document.querySelector('#price');
+// для синхронизации времени въезда и выезда
+var checkOutList = document.querySelector('#timeout').querySelectorAll('option');
+var checkOutTime = document.querySelector('#timeout');
+var checkInTime = document.querySelector('#timein');
+var checkInList = document.querySelector('#timein').querySelectorAll('option');
 
-// var PlaceType = {
-//   palace: 'Особняк',
-//   flat: 'Квартира',
-//   house: 'Частный дом',
-//   bungalo: 'Бунгало'
-// };
+var PlaceType = {
+  palace: 'Особняк',
+  flat: 'Квартира',
+  house: 'Частный дом',
+  bungalo: 'Бунгало'
+};
 
 var roomOptions = {
   1: [1],
@@ -34,10 +42,19 @@ var roomOptions = {
   100: [0]
 };
 
+var appartmentTypePrice = {
+  bungalo: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000
+};
+
+// генерация случайного числа
 var randomNumber = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
+// моковые данные
 var MOCK = {
   author: {
     avatar: ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png']
@@ -79,6 +96,7 @@ var MOCK = {
   }
 };
 
+// генерирование массива данных для карточек и пинов из моковых данных
 var generateContent = function (array) {
   var arr = [];
   for (var i = 0; i < advertsCount; i++) {
@@ -108,63 +126,101 @@ var generateContent = function (array) {
   return arr;
 };
 
+// удаление карточки после нажатия кнопки ESC
+var onEscButtonPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    map.removeChild(pinCard);
+    document.removeEventListener('keydown', onEscButtonPress);
+  }
+};
+
+//  отрисовка и удаление карточки при клике
+var renderCard = function (array) {
+  // добавляю карточку на страницу
+  pinCard.innerHTML = generatePinCard(array).innerHTML;
+  map.appendChild(pinCard);
+  // удаляю карточку при клике
+  pinCard.querySelector('.popup__close').addEventListener('click', function () {
+    map.removeChild(pinCard);
+    document.removeEventListener('keydown', onEscButtonPress);
+  });
+  // удаляю карточку при нажатии ESC
+  document.addEventListener('keydown', onEscButtonPress);
+};
+
+// генерирование пинов для городской карты
 var generatePins = function (array) {
   var element = mapPin.cloneNode(true);
+  element.tabIndex = 0;
   element.style.left = (array.location.x - 25) + 'px';
   element.style.top = (array.location.y - 70) + 'px';
   element.querySelector('img').src = array.author.avatar;
   element.querySelector('img').alt = array.offer.title;
+  element.addEventListener('click', function () {
+    renderCard(array);
+  });
+  element.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      renderCard(array);
+    }
+  });
   fragment.appendChild(element);
 };
 
-// var generateCards = function (card) {
-//   var element = pinCard.cloneNode(true);
-//   element.querySelector('.popup__title').textContent = card.offer.title;
-//   element.querySelector('.popup__text--address').textContent = card.offer.address;
-//   element.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночь';
-//   element.querySelector('.popup__type').textContent = PlaceType[card.offer.type];
-//   element.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
-//   element.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-//   element.querySelector('.popup__features').innerHTML = '';
-//   card.offer.features.forEach(function (it) {
-//     var featureElement = document.createElement('li');
-//     featureElement.className = 'popup__feature popup__feature--' + it;
-//     element.querySelector('.popup__features').appendChild(featureElement);
-//   });
-//   element.querySelector('.popup__description').textContent = card.offer.description;
-//   element.querySelector('.popup__photos').innerHTML = '';
-//   card.offer.photos.forEach(function (it) {
-//     var el = cardPhoto.cloneNode(true);
-//     el.src = it;
-//     element.querySelector('.popup__photos').appendChild(el);
-//   });
+// генерирование содержимого карточки из массива
+var generatePinCard = function (card) {
+  var element = pinCard.cloneNode(true);
+  element.querySelector('.popup__title').textContent = card.offer.title;
+  element.querySelector('.popup__text--address').textContent = card.offer.address;
+  element.querySelector('.popup__text--price').textContent = card.offer.price + ' ₽/ночь';
+  element.querySelector('.popup__type').textContent = PlaceType[card.offer.type];
+  element.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  element.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  element.querySelector('.popup__features').innerHTML = '';
+  card.offer.features.forEach(function (it) {
+    var featureElement = document.createElement('li');
+    featureElement.className = 'popup__feature popup__feature--' + it;
+    element.querySelector('.popup__features').appendChild(featureElement);
+  });
+  element.querySelector('.popup__description').textContent = card.offer.description;
+  element.querySelector('.popup__photos').innerHTML = '';
+  card.offer.photos.forEach(function (it) {
+    var el = cardPhoto.cloneNode(true);
+    el.src = it;
+    element.querySelector('.popup__photos').appendChild(el);
+  });
 
-//   element.querySelector('.popup__avatar').src = card.author.avatar;
-//   return element;
-// };
+  element.querySelector('.popup__avatar').src = card.author.avatar;
+  return element;
+};
 
 var mockArray = generateContent(MOCK);
 
+// отключение полей ввода в массиве
 var disableFieldset = function (fieldset) {
   for (var i = 0; i < fieldset.length; i++) {
     fieldset[i].disabled = true;
   }
 };
 
+// включение полей ввода в массиве
 var enableFieldset = function (fieldset) {
   for (var i = 0; i < fieldset.length; i++) {
     fieldset[i].disabled = false;
   }
 };
 
+// удаление класса у элемента
 var removeClass = function (element, classname) {
   element.classList.remove(classname);
 };
 
+// получение координат пина
 var getPinCoordinates = function (pin) {
   return Math.floor(pin.getBoundingClientRect().left + PIN_RADIUS) + ',' + Math.floor(pin.getBoundingClientRect().top + PIN_HEIGTH);
 };
 
+// расчёт количества гостей под количество комнат
 var calculateGuestsNumber = function (value) {
   capacityList.forEach(function (option) {
     option.disabled = true;
@@ -176,6 +232,33 @@ var calculateGuestsNumber = function (value) {
         opt.selected = true;
       }
     });
+  });
+};
+
+// изменение минимальной цены в зависимости от типа аппартаментов
+var changeMinPrice = function () {
+  roomTypeList.forEach(function (it) {
+    if (it.value === document.querySelector('#type').value) {
+      roomPrice.min = appartmentTypePrice[it.value];
+      roomPrice.placeholder = appartmentTypePrice[it.value];
+    }
+  });
+};
+
+// синхронизация времени въезда и выезда
+var timeSyncOut = function () {
+  checkInList.forEach(function (it) {
+    if (it.value === checkOutTime.value) {
+      it.selected = true;
+    }
+  });
+};
+
+var timeSyncIn = function () {
+  checkOutList.forEach(function (it) {
+    if (it.value === checkInTime.value) {
+      it.selected = true;
+    }
   });
 };
 
@@ -193,7 +276,6 @@ mapPinMain.addEventListener('mousedown', function () {
   pinList.appendChild(fragment);
   addressInput.value = getPinCoordinates(mapPinMain);
   addressInput.readOnly = true;
-  // filtersContainer.insertAdjacentElement('beforebegin', generateCards(mockArray[1]));
 });
 
 mapPinMain.addEventListener('keydown', function (evt) {
@@ -204,12 +286,21 @@ mapPinMain.addEventListener('keydown', function (evt) {
     pinList.appendChild(fragment);
     addressInput.value = getPinCoordinates(mapPinMain);
     addressInput.readOnly = true;
-    // filtersContainer.insertAdjacentElement('beforebegin', generateCards(mockArray[1]));
   }
 });
 
 calculateGuestsNumber(roomNumber.value);
 
+changeMinPrice();
+
+// timeSync();
+
 roomNumber.addEventListener('change', function (evt) {
   calculateGuestsNumber(evt.target.value);
 });
+
+roomType.addEventListener('change', changeMinPrice);
+
+checkInTime.addEventListener('change', timeSyncIn);
+
+checkOutTime.addEventListener('change', timeSyncOut);
